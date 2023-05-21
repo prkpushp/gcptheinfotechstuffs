ec4dbde95a6413544428ddd681782b3b4ec38127
prkpushp <prkpushp@gmail.com>
2023-05-21 16:35:14 +0530

    Initial COmmit - theinfotechstuffs

--- instances.tf
+++ instances.tf
@@ -1,27 +1,9 @@
-
-resource "google_compute_instance" "windows_instance" {
-  name         = "windows-instance"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "windows-server-2022-dc-v20230510"
-    }
-  }
-
-  network_interface {
-    network       = google_compute_network.my_network.id
-    subnetwork    = google_compute_subnetwork.windows_subnet.name
-  }
-
-  metadata_startup_script = <<-EOF
-    #!/bin/bash
-    # Add your startup script commands here
-  EOF
+resource "google_compute_address" "theinfotechstuffs_public_ip" {
+  name         = "theinfotechstuffs-public-ip"
+  address_type = "EXTERNAL"
 }
-
-resource "google_compute_instance" "linux_instance" {
-  name         = "linux-instance"
+resource "google_compute_instance" "theinfotechstuffs_instance" {
+  name         = "theinfotechstuffs"
   machine_type = "n1-standard-1"
   zone         = "us-central1-b"
   boot_disk {
@@ -32,7 +14,7 @@ resource "google_compute_instance" "linux_instance" {
 
   network_interface {
     network       = google_compute_network.my_network.id
-    subnetwork    = google_compute_subnetwork.linux_subnet.name
+    subnetwork    = google_compute_subnetwork.theinfotechstuffs_subnet.name
   }
 
   metadata_startup_script = <<-EOF
@@ -41,10 +23,7 @@ resource "google_compute_instance" "linux_instance" {
   EOF
 }
 
-resource "google_compute_address" "windows_public_ip" {
-  name         = "windows-public-ip"
-  address_type = "EXTERNAL"
-}
+
 
 resource "google_compute_instance" "windows_public_instance" {
   name         = "windows-public-instance"
--- networks.tf
+++ networks.tf
@@ -1,17 +1,17 @@
-resource "google_compute_network" "infotech_network" {
-  name                    = "infotech-network"
+resource "google_compute_network" "theinfotechstuffs_network" {
+  name                    = "theinfotechstuffs-network"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "web_subnet" {
-  name          = "web-subnet"
+resource "google_compute_subnetwork" "theinfotechstuffs_subnet" {
+  name          = "theinfotechstuffs-subnet"
   ip_cidr_range = "10.10.1.0/24"
   network       = google_compute_network.my_network.self_link
   region        = "us-central1"
 }
 
-resource "google_compute_firewall" "allow_ssh_web" {
-  name    = "allow-ssh-web"
+resource "google_compute_firewall" "allow_ssh_theinfotechstuffs" {
+  name    = "allow-ssh-theinfotechstuffs"
   network = google_compute_network.my_network.name
 
   allow {
